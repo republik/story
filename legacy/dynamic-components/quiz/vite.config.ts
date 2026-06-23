db5fd119dadd19aa7fcf0a6f8b40459448105c32
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-06-23 15:54:06 +0200

    formatting

--- legacy/dynamic-components/quiz/vite.config.ts
+++ legacy/dynamic-components/quiz/vite.config.ts
@@ -1,32 +1,15 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  define: { "process.env.NODE_ENV": '"production"' },
-  resolve: {
-    alias: [
-      {
-        find: /^@story\/theme\/fonts\.css/,
-        replacement: path.resolve(__dirname, "./theme/fonts.css"),
-      },
-      {
-        find: /^@story\/theme\/presets$/,
-        replacement: path.resolve(__dirname, "./theme/presets.ts"),
-      },
-      {
-        find: /^@story\/theme/,
-        replacement: path.resolve(__dirname, "./generated"),
-      },
-    ],
-  },
+  define: { 'process.env.NODE_ENV': '"production"' },
   build: {
     lib: {
-      entry: "./src/index.js",
-      formats: ["es"],
-      fileName: "index",
+      entry: './src/index.js',
+      formats: ['es'],
+      fileName: 'index',
     },
     copyPublicDir: false,
   },
   plugins: [react()],
-});
+})
