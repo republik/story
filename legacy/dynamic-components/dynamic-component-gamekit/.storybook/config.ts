cf7113fceec10084865c1315a1708925937284a6
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-06-30 11:21:48 +0200

    add list of dynamic components for reference, remove some obviously unused ones

--- legacy/dynamic-components/dynamic-component-gamekit/.storybook/config.ts
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-import 'glamor/reset';
-const req = require.context('../src', true, /\.story\.tsx$/);
-
-function loadStories() {
-  req.keys().forEach(req);
-}
-
-configure(loadStories, module);
--- legacy/dynamic-components/dynamic-component-gamekit/src/theme.ts
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-  bgPrimary: '#999',
-  bgSecondary: '#999',
-  textPrimary: '#fff',
-  textSecondary: '#fff',
-}
