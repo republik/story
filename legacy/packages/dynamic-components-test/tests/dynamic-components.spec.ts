eea0c5c32a5b3917946bd3bca9104d1c6845d8db
Jeremy Stucki <jeremy.stucki@gmail.com>
2026-07-02 12:02:00 +0200

    improve test script

--- legacy/packages/dynamic-components-test/tests/dynamic-components.spec.ts
+++ legacy/packages/dynamic-components-test/tests/dynamic-components.spec.ts
@@ -2,29 +2,62 @@ import { expect, test } from '@playwright/test'
 
 import usageData from '../../../dynamic-component-usage.json'
 
-const slugs = usageData.map((entry) => `/articles${entry.slug}`)
+const pagesByComponent: Map<string, string[]> = new Map()
 
-for (const slug of slugs) {
-  test(slug, async ({ page }) => {
-    const errors: string[] = []
+for (const entry of usageData) {
+  const path = `/articles${entry.slug}`
+  for (const src of entry.src) {
+    const url = new URL(src)
+    const key = url.pathname
+    if (pagesByComponent.has(key)) {
+      pagesByComponent.set(key, [
+        ...new Set([...pagesByComponent.get(key)!, path]),
+      ])
+    } else {
+      pagesByComponent.set(key, [path])
+    }
+  }
+}
 
-    page.on('pageerror', (err) => {
-      errors.push(err.message)
-    })
+for (const [component, paths] of pagesByComponent.entries()) {
+  const match = component.match(
+    /\/dynamic-components\/([a-z0-9\-]+)\/([a-z0-9\-]+)\.js/,
+  )
+  const componentName = match
+    ? match[2] === 'index'
+      ? match[1]
+      : `${match[1]}-${match[2]}`
+    : component
 
-    page.on('console', (msg) => {
-      if (msg.type() === 'error') {
-        errors.push(msg.text())
-      }
-    })
+  test.describe(componentName, () => {
+    for (const path of paths) {
+      test(`${componentName}:${path}`, async ({ page }) => {
+        const errors: string[] = []
 
-    const response = await page.goto(slug)
+        page.on('pageerror', (err) => {
+          errors.push(err.message)
+        })
 
-    expect(response?.status(), `HTTP error for ${slug}`).toBeLessThan(400)
-    await page.waitForLoadState('networkidle')
+        page.on('console', (msg) => {
+          if (msg.type() === 'error') {
+            errors.push(msg.text())
+          }
+        })
 
-    expect(errors, `JS errors on ${slug}:\n${errors.join('\n')}`).toHaveLength(
-      0,
-    )
+        await page.goto(path)
+
+        for (const component of await page
+          .locator(`[data-dynamic-component=${componentName}]`)
+          .all()) {
+          await expect(component.locator('[data-state="error"]')).toBeHidden()
+          await expect(component.locator('[data-state="loading"]')).toBeHidden()
+        }
+
+        expect(
+          errors,
+          `JS errors on ${path}:\n${errors.join('\n')}`,
+        ).toHaveLength(0)
+      })
+    }
   })
 }
